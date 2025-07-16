2c7f88e5edbff030b2f758c829cdadcd3a1bb6b4
sarderibrahim <77164653+sarderibrahim@users.noreply.github.com>
2025-07-16 22:19:14 +0200

    Update index.js

--- Build an App with Firebase/index.js
+++ Build an App with Firebase/index.js
@@ -1,1 +1,8 @@
+const inputFieldElement = document.getElementById("inputField")
+const addButtonElement = document.getElementById("addButton")
 
+addButtonElement.AddEventListener("click", function() {
+  let inputElementValue = inputFieldElement.value
+
+  console.log(inputElementValue)
+})
